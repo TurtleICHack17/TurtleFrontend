0f2de62188b4c586be7e0da03fca5c70161cfcf3
Heiki Riisikamp <hr1514@ic.ac.uk>
2017-02-04 22:25:02 +0000

    video playback comp

--- client/components/RecordingPage/index.js
+++ client/components/RecordingPage/index.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react'
 import style from './style.css'
-import { captureUserMedia, S3Upload } from './AppUtils';
+import {captureUserMedia, S3Upload,} from './AppUtils';
+import RaisedButton from 'material-ui/RaisedButton';
 import RecordRTC from 'recordrtc';
 
-const hasGetUserMedia = !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
-                        navigator.mozGetUserMedia || navigator.msGetUserMedia);
+const hasGetUserMedia = !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
 
 class RecordingPage extends React.Component {
   constructor(props) {
@@ -14,7 +14,7 @@ class RecordingPage extends React.Component {
       recordVideo: null,
       src: null,
       uploadSuccess: null,
-      uploading: false
+      uploading: false,
     };
 
     this.requestUserMedia = this.requestUserMedia.bind(this);
@@ -23,7 +23,7 @@ class RecordingPage extends React.Component {
   }
 
   componentDidMount() {
-    if(!hasGetUserMedia) {
+    if (!hasGetUserMedia) {
       alert("Your browser cannot stream from your webcam. Please switch to Chrome or Firefox.");
       return;
     }
@@ -33,14 +33,14 @@ class RecordingPage extends React.Component {
   requestUserMedia() {
     console.log('requestUserMedia')
     captureUserMedia((stream) => {
-      this.setState({ src: window.URL.createObjectURL(stream) });
+      this.setState({src: window.URL.createObjectURL(stream)});
       console.log('setting state', this.state)
     });
   }
 
   startRecord() {
     captureUserMedia((stream) => {
-      this.state.recordVideo = RecordRTC(stream, { type: 'video' });
+      this.state.recordVideo = RecordRTC(stream, {type: 'video'});
       this.state.recordVideo.startRecording();
     });
   }
@@ -50,38 +50,48 @@ class RecordingPage extends React.Component {
       let params = {
         type: 'video/webm',
         data: this.state.recordVideo.blob,
-        id: Math.floor(Math.random()*90000) + 10000
+        id: Math.floor(Math.random() * 90000) + 10000,
       }
 
-      this.setState({ uploading: true });
+      this.setState({uploading: true});
 
-      S3Upload(params)
-      .then((success) => {
+      S3Upload(params).then((success) => {
         console.log('enter then statement')
-        if(success) {
+        if (success) {
           console.log(success)
-          this.setState({ uploadSuccess: true, uploading: false });
+          this.setState({uploadSuccess: true, uploading: false,});
         }
       }, (error) => {
         alert(error, 'error occurred. check your aws settings and try again.')
-      })
-      .catch((err) => {
+      }).catch((err) => {
         console.log(err);
       })
     });
   }
 
   render() {
-    return(
-      <div>
-        {this.state.uploadSuccess ? 'Upload success!' : ''}
+    return (
+      <div className={style.main}>
         <div>
-          <video autoPlay muted src={this.state.src} />
+          <video autoPlay muted src={this.state.src}/>
+        </div>
+        <div>
+          {this.state.uploading
+            ? <div>Uploading...</div>
+            : null
+}
+          {this.state.uploadSuccess
+            ? 'Upload success!'
+            : ''}
+        </div>
+        <div className={style.padding}>
+          <div className={style.inline}>
+            <RaisedButton primary={true} onClick={this.startRecord} label="Start Record"/>
           </div>
-        {this.state.uploading ?
-          <div>Uploading...</div> : null}
-        <div><button onClick={this.startRecord}>Start Record</button></div>
-        <div><button onClick={this.stopRecord}>Stop Record</button></div>
+          <div className={style.inline}>
+            <RaisedButton primary={true} onClick={this.stopRecord} label="Stop Record"/>
+          </div>
+        </div>
       </div>
     )
   }
