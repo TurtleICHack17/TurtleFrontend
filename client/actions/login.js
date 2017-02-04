dff635da2e69f0425a217849293849bb84c0268c
najiji <thomas.debrunner@hotmail.com>
2017-02-04 15:38:08 +0000

    facebook login

--- client/actions/login.js
+++ client/actions/login.js
@@ -1,8 +1,19 @@
 import { createAction } from 'redux-actions'
+import 'whatwg-fetch'
+
 
 export function facebookOnResponse(response) {
   // succesfull
   if (response.hasOwnProperty('userID')) {
+
+    // send facebook object to our API
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(response)
+    })
 
     return {
         type: 'set facebook success',
