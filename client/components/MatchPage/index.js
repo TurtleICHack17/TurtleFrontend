e038f74102256d6e4e25f73c4e8535986bcf4bb5
najiji <thomas.debrunner@hotmail.com>
2017-02-05 10:45:34 +0000

    restyle match page

--- client/components/MatchPage/index.js
+++ client/components/MatchPage/index.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react'
 import style from './style.css'
+import Avatar from 'material-ui/Avatar';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class MatchPage extends Component {
   render() {
     const matchId = this.props.params.matchId;
+    const fbLink = "https://facebook.com/" + matchId
     return (
       <div className={style.main}>
-        <h1 className=''>It's a match!</h1>
-        <h2>Your match: {matchId}</h2>
-        <img src={`http://graph.facebook.com/${matchId}/picture`} />
+        <Avatar size={220} src={`http://graph.facebook.com/${matchId}/picture?type=large`} className={style.avatar}></Avatar>
+        <h1 className="">It's a match!</h1>
+        <h2>Don't be shy!</h2>
+        <RaisedButton label="Connect on facebook!" primary={true} href={fbLink}  />
+
       </div>
     );
   }
